68b89350ccaf3bb5ac85043c7ea98d9b33544be4
AlwaysJed <161187377+AlwaysJed@users.noreply.github.com>
2024-10-11 19:22:52 -0800

    Update ExtractingPersonalrec.js
    Async Function and Async Function

--- ToGetData/ExtractingPersonalrec.js
+++ ToGetData/ExtractingPersonalrec.js
@@ -1,1 +1,31 @@
+onst token = '';
+
+const fs = require('fs'); 
+
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    method,
+    ...(body && { body: JSON.stringify(body) }) 
+  });
+
+  if (!res.ok) {
+    const errorResponse = await res.json();
+    throw new Error(`Error: ${res.status} ${errorResponse.message}`);
+  }
+
+  return await res.json();
+}
+
+const topTracksIds = [ "place your ids u got here from the first program"
+];
+
+async function getRecommendations() {
+  return (await fetchWebApi(
+    `v1/recommendations?limit=5&seed_tracks=${topTracksIds.join(',')}`, 'GET'
+  )).tracks;
+}
 
